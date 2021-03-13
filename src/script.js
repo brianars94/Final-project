d8315e9a0d2160cda62e8dce653abe549a761fa2
brianars94 <brianars@hawaii.edu>
2021-03-13 11:16:28 -0600

    fixed the icon to show

--- src/script.js
+++ src/script.js
@@ -48,6 +48,7 @@ let todaysDate = date.getDate();
 
 
 function displayWeatherCondition(response) {
+  let iconElement = document.querySelector("#icon");
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -66,6 +67,11 @@ function displayWeatherCondition(response) {
 
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+    iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
